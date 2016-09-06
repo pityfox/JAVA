07acf6be6fa5eec83d9f0fb443e7c313c1ded863
pityfox <pityfox@gmail.com>
2016-09-06 11:41:53 +0200

    New JAva

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("STFU");
+//		Scanner lol=new Scanner(System.in);
+//		String entree=lol.nextLine();
+//		System.out.println(entree);
+		
+		methodMan();
+	}
+	
+	static void methodMan() {
+		int a = 10;
+		String b = "lrgkngjerngej";
+		System.out.println(b+a);
+		
+		int x = 5;
+		int y = ++x;
+		System.out.println(y);
+	}
+	
+	static void redMan() {
+		Scanner entree= new Scanner(System.in);
+		String nom = entree.nextLine();
+	}
+
+}
