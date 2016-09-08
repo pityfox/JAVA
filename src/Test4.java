89387c6a1fdd1a1c36b76ff0ab7d5713887eca10
pityfox <pityfox@gmail.com>
2016-09-08 12:21:23 +0200

    Retourner plus d'une valeur, Modification chaines caract

--- src/Test4.java
+++ src/Test4.java
@@ -7,9 +7,17 @@ public class Test4 {
 		// TODO Auto-generated method stub
 		//testV2(creerTableau());
 		
-		test1();
+		// Méthodes imbriquées
+		//test1();
+		
+		// Retourner plus d'une valeur, oupas
+		//testRetour();
+		
+		// Modification de Strings
+		//testReverse();
 	}
 	
+	// Saisie d'une chaine de caractere
 	static String saisirString(String demande) {
 		System.out.println(demande);
 		Scanner saisie=new Scanner(System.in);
@@ -17,6 +25,7 @@ public class Test4 {
 		return a;
 	}
 	
+	// Saisie d'un entier
 	static int saisirNombre(String demande) {
 		System.out.println(demande);
 		Scanner saisie=new Scanner(System.in);
@@ -25,6 +34,7 @@ public class Test4 {
 		
 	}
 	
+	// Saisie d'un booléen
 	static boolean saisirBool(String demande){
 		System.out.println(demande);
 		Scanner saisie=new Scanner(System.in);
@@ -32,6 +42,7 @@ public class Test4 {
 		return nbr;
 	}
 	
+	// Creer un tableau de val random en saisissant sa taille
 	static int[] creerTableau(){
 		int taille = saisirNombre("Taille du tableau :");
 		int[] tab = new int[taille];
@@ -43,6 +54,7 @@ public class Test4 {
 		return tab;
 	}
 	
+	// Affiche le contenu d'un tableau
 	static void afficheTableau(int[] x){
 		for(int s:x){
 			System.out.print(s+" ");
@@ -50,6 +62,7 @@ public class Test4 {
 		System.out.println();
 	}
 	
+	// Double les valeurs d'un tableau dans un nouveau
 	static int[] doublerTableauV1(int[] x){
 		int[] newTab = new int[x.length];
 		for(int i=0;i<newTab.length;i++){
@@ -58,18 +71,21 @@ public class Test4 {
 		return newTab;
 	}
 	
+	// Double les valeurs d'un tableau directement
 	static void doublerTableauV2(int[] tab){
 		for(int i=0;i<tab.length;i++){
 			tab[i] = tab[i]*2;
 		}
 	}
 	
+	// Double un tableau en entré et affiche avant apres
 	static void testV2(int[] tab){
 		afficheTableau(tab);
 		doublerTableauV2(tab);
 		afficheTableau(tab);
 	}
 	
+	// méthodes imbriquées saisie infos
 	static void test1(){
 		String nom = saisirString("Votre nom :");
 		String prenom = saisirString("Votre prénom :");
@@ -81,10 +97,12 @@ public class Test4 {
 		System.out.println(reponse);
 	}
 	
+	// Concaténation
 	static String test2(String nom, String prenom, Boolean homme){
 		return ((homme)?"Mr":"Mme")+" "+prenom+" "+nom;
 	}
 	
+	// Concaténation et booleen en test ternaire
 	static String test3(String nomComplet, int age){
 		String etat="";
 		if(age >= 18){
@@ -94,5 +112,44 @@ public class Test4 {
 		}
 		
 		return nomComplet+" "+age+" ans, "+etat+".";
+	}
+	
+	// Saisie 2 val et affiche somme et difference
+	static void testRetour(){
+		int val1 = saisirNombre("Valeur 1:");
+		int val2 = saisirNombre("Valeur 2:");
+
+		int[] tab = new int[4];
+		tab[0] = val1;
+		tab[1] = val2;
+		compute(tab);
+		System.out.println("Somme : "+tab[2]);
+		System.out.println("Différence : "+tab[3]);
+	}
+	
+	// Calcul sum et diff et ajoute au tableau
+	static void compute(int[] tab){
+		tab[2] = tab[0]+tab[1];
+		tab[3] = tab[0]-tab[1];
+	}
+	
+	// Inverser l'ordre des mots et mise en majuscules
+	static void testReverse(){
+		String phrase = saisirString("Ecrivez une phrase :");
+		String[] tab = phrase.split(" ");
+		reverseCaps(tab);
+		System.out.println(String.join(" ",tab));
+		//for(String el:tab)System.out.print(el+" ");
+	}
+	
+	// Inverser l'ordre d'un tableau et mise en majuscules
+	static void reverseCaps(String[] tab){
+		String[] tab2 = new String[tab.length];
+		for(int i=0;i<tab.length;i++){
+			tab2[tab.length-1-i] = tab[i];
+		}
+		for(int i=0;i<tab.length;i++){
+			tab[i] = tab2[i].toUpperCase();
+		}
 	}
 }
