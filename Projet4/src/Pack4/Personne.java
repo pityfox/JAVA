5fc6c5b4f016cd5fd8e5a33016f3eb978e46d2a2
pityfox <pityfox@gmail.com>
2016-09-09 11:00:51 +0200

    added Projet4, changed folders hierarchy

---
+++ Projet4/src/Pack4/Personne.java
@@ -0,0 +1,20 @@
+package Pack4;
+
+public class Personne {
+
+	String nom;
+	String prenom;
+	int age;
+	
+	Personne()
+	{
+		nom="Dupond";
+	}
+	
+	void affiche()
+	{
+		int a=0;
+		System.out.println(nom+" "+prenom+" "+age);
+	}
+	
+}
---
+++ Projet4/src/Pack4/TestPersonne.java
@@ -0,0 +1,34 @@
+package Pack4;
+
+public class TestPersonne {
+
+	public static void main(String[] args) {
+		
+		test();
+		
+	}
+
+	static void test(){
+		Personne p1=new Personne();
+		p1.nom="toto";
+		p1.prenom="titi";
+		p1.age=10;
+		
+		System.out.println(p1.nom+" "+p1.prenom+" "+p1.age);
+		
+		Personne p2=new Personne();
+		p2.nom="aa";
+		p2.prenom="bb";
+		p2.age=20;
+		
+		System.out.println(p2.nom+" "+p2.prenom+" "+p2.age);
+		
+		Personne p3=p1;
+		System.out.println(p3.nom+" "+p3.prenom+" "+p3.age);
+
+//		p1=null;
+//		System.out.println(p1.nom+" "+p1.prenom+" "+p1.age);
+		
+		p3.affiche();
+	}
+}
