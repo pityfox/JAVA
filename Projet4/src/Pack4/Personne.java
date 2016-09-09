8597d87ac7c3efef3f7dc6b314aca2dd4702ad70
pityfox <pityfox@gmail.com>
2016-09-09 12:04:09 +0200

    Constructeurs d'initialisation et de copie

--- Projet4/src/Pack4/Personne.java
+++ Projet4/src/Pack4/Personne.java
@@ -2,15 +2,47 @@ package Pack4;
 
 public class Personne {
 
-	String nom;
+	// Instanciation des valeurs
+	String nom="abc";
 	String prenom;
 	int age;
 	
+	// Constructeur par défaut sans paramètres
 	Personne()
 	{
-		nom="Dupond";
+		this.nom="Dupond";
 	}
 	
+	//Constructeurs d'initialisation, paramètres passés au constructeur
+	Personne(String nom)
+	{
+		this.nom=nom;
+	}
+	
+	Personne(String nom, String prenom)
+	{
+		this.nom=nom;
+		this.prenom=prenom;
+	}
+	
+	Personne(String nom, String prenom, int age)
+	{
+		// appel d'un constructeur
+		this(nom,prenom);
+//		this.nom=nom;
+//		this.prenom=prenom;
+		this.age=age;
+	}
+	
+	// Copie Constructeur
+	Personne(Personne x)
+	{
+		this.nom=x.nom;
+		this.prenom=x.prenom;
+		this.age=x.age;
+	}
+	
+	// Méthodes
 	void affiche()
 	{
 		int a=0;
--- Projet4/src/Pack4/TestPersonne.java
+++ Projet4/src/Pack4/TestPersonne.java
@@ -9,26 +9,42 @@ public class TestPersonne {
 	}
 
 	static void test(){
-		Personne p1=new Personne();
-		p1.nom="toto";
-		p1.prenom="titi";
-		p1.age=10;
-		
-		System.out.println(p1.nom+" "+p1.prenom+" "+p1.age);
-		
-		Personne p2=new Personne();
-		p2.nom="aa";
-		p2.prenom="bb";
-		p2.age=20;
-		
-		System.out.println(p2.nom+" "+p2.prenom+" "+p2.age);
-		
-		Personne p3=p1;
-		System.out.println(p3.nom+" "+p3.prenom+" "+p3.age);
-
-//		p1=null;
+//		Personne p1=new Personne();
+//		p1.nom="toto";
+//		p1.prenom="titi";
+//		p1.age=10;
+//		
 //		System.out.println(p1.nom+" "+p1.prenom+" "+p1.age);
+//		
+//		Personne p2=new Personne();
+//		p2.nom="aa";
+//		p2.prenom="bb";
+//		p2.age=20;
+//		
+//		System.out.println(p2.nom+" "+p2.prenom+" "+p2.age);
+//		
+//		Personne p3=p1;
+//		System.out.println(p3.nom+" "+p3.prenom+" "+p3.age);
+//
+////		p1=null;
+////		System.out.println(p1.nom+" "+p1.prenom+" "+p1.age);
+//		
+//		p3.affiche();
 		
+		Personne p1 = new Personne();
+		p1.affiche();
+		
+		Personne p2 = new Personne("Boulet");
+		p2.affiche();
+		
+		Personne p3 = new Personne("Jean", "Michel");
 		p3.affiche();
+		
+		Personne p4 = new Personne("Michel", "Blob", 42);
+		p4.affiche();
+		
+		Personne copie = new Personne(p3);
+		copie.affiche();
+		
 	}
 }
